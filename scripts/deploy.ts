54b1737a398af8f04d9585d5c7ff583e81467afb
Cesar Varela <mail@cesarvarela.com>
2022-05-29 00:31:21 -0300

    Update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,20 +6,17 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [owner] = await ethers.getSigners();
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Sandwich = await ethers.getContractFactory("Sandwich");
 
-  await greeter.deployed();
+  // const gas = await ethers.provider.estimateGas(Sandwich.getDeployTransaction(owner.address));
 
-  console.log("Greeter deployed to:", greeter.address);
+  const sandwich = await Sandwich.deploy(owner.address);
+
+  await sandwich.deployed();
+
+  console.log("Sandwich deployed to:", sandwich.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
